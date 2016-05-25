b709e9b75db6ebf1b7fc4fd6273c2653741ce7bf
Martin Florian <florian.martin@insa-rouen.fr>
2016-05-25 13:09:20 +0200

    Avancement de l'interface

--- src/main/java/uml/dutiens/org/MyUI.java
+++ src/main/java/uml/dutiens/org/MyUI.java
@@ -9,6 +9,8 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Alignment;
@@ -37,13 +39,20 @@ public class MyUI extends UI {
 
 	private static final Logger log = LoggerFactory.getLogger(MyUIServlet.class);
 
-	private VerticalLayout layout = new VerticalLayout();
-	private HorizontalLayout menuBar = new HorizontalLayout();
-	private CssLayout menuBarRight = new CssLayout();
-	private Label nomProj = new Label("SalleOp") ;
-	private TextField tfRecherche = new TextField();
-	private Button bRecherche = new Button("Recherche") ;
+	private final VerticalLayout layout = new VerticalLayout();
+	private final HorizontalLayout menuBar = new HorizontalLayout();
+	private final CssLayout menuBarRight = new CssLayout();
+	private final Label nomProj = new Label("SalleOp") ;
+	private final TextField tfRecherche = new TextField();
+	private final Button bRecherche = new Button("Recherche") ;
 
+
+	private final GridLayout grid = new GridLayout(1, 2);
+	private final VerticalLayout left = new VerticalLayout();
+	private final VerticalLayout right = new VerticalLayout();
+	private final HorizontalLayout bottom = new HorizontalLayout();
+
+	private final Panel informations = new Panel("Informations");
 
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
@@ -53,7 +62,8 @@ public class MyUI extends UI {
 		// Barre de menu
 		menuBar.addComponent(nomProj);
 		menuBar.addComponent(menuBarRight);
-		menuBarRight.setStyleName(" element-align-right");
+		menuBarRight.setStyleName(" element-align-middle element-align-right");
+		nomProj.setStyleName(" element-align-middle");
 
 		menuBarRight.addComponent(tfRecherche);
 		menuBarRight.addComponent(bRecherche);
@@ -66,7 +76,25 @@ public class MyUI extends UI {
 
 		// Layout principal
 		layout.addComponent(menuBar);
+		layout.addComponent(grid);
+		layout.addComponent(bottom);
 		layout.setStyleName(" main-layout");
+
+		grid.addComponent(left, 0, 0);
+		grid.addComponent(right, 0, 1);
+		grid.setWidth("80%");
+
+		// Panel informations
+		left.addComponent(informations);
+
+		// Annonce
+		// Titre
+		
+		// Texte
+		
+		// Bouton réservation
+		
+		// Galerie photos
 
 		setContent(layout);
 	}
